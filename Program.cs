a31568f7cdf4745e46eaaf4439b6718bc61ab9d6
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-02 16:06:44 +0300

    3rd task is done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿/*
-Задача 66: Задайте значения M и N.
- Напишите программу, которая найдёт сумму натуральных 
- элементов в промежутке от M до N.
+Задача 68: Напишите программу вычисления
+ функции Аккермана с помощью рекурсии. Даны 
+ два неотрицательных числа m и n.
 
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
+m = 2, n = 3 -> A(m,n) = 29
 */
 
 using static System.Console;
@@ -13,30 +12,21 @@ Clear();
 
 int m = InputNumbers("Enter m: ");
 int n = InputNumbers("Enter n: ");
-int temp = m;
 
-if (m > n) 
+int functionAkkerman = Ack(m, n);
+
+Write($"Function of Akkerman is  {functionAkkerman} ");
+
+int Ack(int m, int n)
 {
-  m = n; 
-  n = temp;
-}
-
-PrintSumm(m, n, temp=0);
-
-void PrintSumm(int m, int n, int summ)
-{
-  summ = summ + n;
-  if (n <= m)
-  {
-    Write($"Sum of elements is {summ} ");
-    return;
-  }
-  PrintSumm(m, n - 1, summ);
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
 {
   Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
+  int output = Convert.ToInt32(ReadLine());
   return output;
 }
